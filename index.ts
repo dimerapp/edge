120e23abe55980c15a266158dfce7991912ddcb8
Harminder Virk <virk.officials@gmail.com>
2023-04-05 21:38:33 +0530

    style: update license block

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 /*
  * @dimerapp/edge
  *
- * (c) Harminder Virk <virk@adonisjs.com>
+ * (c) DimerApp
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
